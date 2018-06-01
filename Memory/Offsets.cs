f40bcea0d7c8eb35443a73ded74b43279d467fff
mastahg <mastahg@users.noreply.github.com>
2018-06-01 14:29:30 -0400

    Update patterns

--- Memory/Offsets.cs
+++ Memory/Offsets.cs
@@ -22,13 +22,16 @@ namespace Deep.Memory
 #pragma warning disable CS0649
     internal static class Offsets
     {
-        [Offset("Search 0F B6 81 ? ? ? ? 44 0F B6 CA Add 3 Read32")] //0x187A```````
+        [Offset("Search 0F B6 81 ? ? ? ? 49 8D 95 ? ? ? ? Add 3 Read32")] //0x1BEA
+        [OffsetCN("Search 0F B6 81 ? ? ? ? 44 0F B6 CA Add 3 Read32")] //0x187A
         internal static int DDMapGroup;
 
-        [Offset("Search 48 8D 91 ? ? ? ? 48 C7 84 24 ? ? ? ? ? ? ? ? Add 3 Read32")] //0x11c0
+        [Offset("Search 49 8D 95 ? ? ? ? 48 69 C8 ? ? ? ?  Add 3 Read32")]
+        [OffsetCN("Search 48 8D 91 ? ? ? ? 48 C7 84 24 ? ? ? ? ? ? ? ? Add 3 Read32")] //0x11c8
         internal static int Map5xStart;
 
-        [Offset("Search 4C 69 C0 ? ? ? ? 48 8D 91 ? ? ? ? Add 3 Read32" )] //0x338
+        [Offset("Search 48 69 C8 ? ? ? ? 48 03 D1 45 Add 3 Read32")]
+        [OffsetCN("Search 4C 69 C0 ? ? ? ? 48 8D 91 ? ? ? ? Add 3 Read32")]//0x338
         internal static int Map5xSize;
 
         [Offset("Search 0F B7 82 ? ? ? ? 2B C8 48 8D 0C 89 Add 3 Read32" )] // 0x332
