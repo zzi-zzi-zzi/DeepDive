37b289f83f8515ef79f027ecdcae77265d459940
mastahg <mastahg@users.noreply.github.com>
2022-08-23 17:06:19 -0400

    Update Offsets.cs
    Patch 6.2 changed the function quite a bit, values seem correct

--- Memory/Offsets.cs
+++ Memory/Offsets.cs
@@ -22,28 +22,28 @@ namespace Deep.Memory
 #pragma warning disable CS0649
     internal static class Offsets
     {
-        [Offset("Search 0F B6 81 ? ? ? ? 49 8D 95 ? ? ? ? Add 3 Read32")] //0x1BEA
-        //[OffsetCN("Search 0F B6 81 ? ? ? ? 44 0F B6 CA Add 3 Read32")] //0x187A
+        [Offset("Search 0F B6 81 ? ? ? ? 4C 8D AE ? ? ? ? Add 3 Read32")]
+        [OffsetCN("Search 0F B6 81 ? ? ? ? 49 8D 95 ? ? ? ? Add 3 Read32")]
         internal static int DDMapGroup;
 
-        [Offset("Search 49 8D 95 ? ? ? ? 48 69 C8 ? ? ? ?  Add 3 Read32")]
-        //[OffsetCN("Search 48 8D 91 ? ? ? ? 48 C7 84 24 ? ? ? ? ? ? ? ? Add 3 Read32")] //0x11c8
+        [Offset("Search 4C 8D AE ? ? ? ? 48 69 C8 ? ? ? ?   Add 3 Read32")]
+        [OffsetCN("Search 49 8D 95 ? ? ? ? 48 69 C8 ? ? ? ?  Add 3 Read32")]
         internal static int Map5xStart;
 
-        [Offset("Search 48 69 C8 ? ? ? ? 48 03 D1 45 Add 3 Read32")]
-        //[OffsetCN("Search 4C 69 C0 ? ? ? ? 48 8D 91 ? ? ? ? Add 3 Read32")]//0x338
+        [Offset("Search 48 69 C8 ? ? ? ? 4C 03 E9 45 33 C9 Add 3 Read32")]
+        [OffsetCN("Search 48 69 C8 ? ? ? ? 48 03 D1 45 Add 3 Read32")]
         internal static int Map5xSize;
 
-        [Offset("Search 0F B7 82 ? ? ? ? 2B C8 48 8D 0C 89 Add 3 Read32" )] // 0x332
+        [Offset("Search 41 0F B7 85 ? ? ? ? 2B C8  Add 4 Read32")]
+        [OffsetCN("Search 0F B7 82 ? ? ? ? 2B C8 48 8D 0C 89 Add 3 Read32")]
         internal static int WallStartingPoint;
 
-        [Offset("Search 49 8D BE ? ? ? ? 0F 1F 44 00 ? 8B 17 Add 3 Read32")] //0x140
+        [Offset("Search 48 81 C3 ? ? ? ? 48 8D 3D ? ? ? ?  Add 3 Read32")]
+        [OffsetCN("Search 49 8D BE ? ? ? ? 0F 1F 44 00 ? 8B 17 Add 3 Read32")]
         internal static int Starting;
 
-        [Offset("Search 41 8B 96 ? ? ? ? 84 C9 Add 3 Read32")]//0x13c
-        internal static int UNK_StartingCircle;
-
-        [Offset("Search 42 0F B6 8C 28 ? ? ? ? 84 0E Add 5 Read32")]//0x1860
+        [Offset("Search F6 84 30 ? ? ? ? ? 74 0F  Add 3 Read32")]
+        [OffsetCN("Search 42 0F B6 8C 28 ? ? ? ? 84 0E Add 5 Read32")]
         internal static int WallGroupEnabled;
     }
 #pragma warning restore CS0649
