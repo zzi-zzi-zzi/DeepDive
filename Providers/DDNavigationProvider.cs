c9f7c78c24e7a885028bfc88055f810b823be650
mastahg <mastahg@users.noreply.github.com>
2025-11-29 05:55:10 -0500

    fix RB_TC

--- Providers/DDNavigationProvider.cs
+++ Providers/DDNavigationProvider.cs
@@ -224,7 +224,11 @@ namespace Deep.Providers
                         // var wall = Core.Memory.Read<uint>(v9 + Offsets.UNK_StartingCircle);
                         //wallset.Add(wall);
 
-                        var @byte = Core.Memory.Read<byte>(director + (2*v5) + PublicOffsets.DeepDungeonOffsets.WallGroupEnabled);
+#if RB_TC
+                        var @byte = Core.Memory.Read<byte>(director + (v5) + PublicOffsets.DeepDungeonOffsets.WallGroupEnabled);
+#else
+                        var @byte = Core.Memory.Read<byte>(director + (2 * v5) + PublicOffsets.DeepDungeonOffsets.WallGroupEnabled);
+#endif 
                         var walls = Core.Memory.ReadArray<uint>(v9 + PublicOffsets.DeepDungeonOffsets.Starting, 4);
                         for (var v16 = 0; v16 < 4; v16++)
                         {
